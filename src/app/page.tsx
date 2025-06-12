58098e78b502a55bdccc8d1f7723798603ecefda
prakashar11 <prakashar@outlook.com>
2025-06-12 17:19:03 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,12 @@
 import Sidebar from '@/components/playground/Sidebar/Sidebar'
 import { ChatArea } from '@/components/playground/ChatArea'
 import { Suspense } from 'react'
-import { useState, useEffect } from 'react'
 
 export default function Home() {
-  const [showToolCalls, setShowToolCalls] = useState<boolean>(true);
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <div className="flex h-screen bg-background/80">
-        <Sidebar showToolCalls={showToolCalls} setShowToolCalls={setShowToolCalls}/>
+        <Sidebar />
         <ChatArea />
       </div>
     </Suspense>
