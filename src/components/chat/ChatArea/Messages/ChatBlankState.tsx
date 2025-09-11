a939bfa24925acaff11a3371241311c7acfb0786
anuragts <anuragsharma011011@gmail.com>
2025-09-11 13:23:03 +0530

    fix: update

--- src/components/chat/ChatArea/Messages/ChatBlankState.tsx
+++ src/components/chat/ChatArea/Messages/ChatBlankState.tsx
@@ -172,7 +172,7 @@ const ChatBlankState = () => {
               </div>
             </span>
           </div>
-          <p>For the full experience, visit the Agent Playground.</p>
+          <p>For the full experience, visit the Agent OS</p>
         </motion.h1>
         <motion.div
           initial={{ opacity: 0, y: 10 }}
