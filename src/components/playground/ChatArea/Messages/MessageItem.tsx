a70c4d7990504fe34d1d6dfdd45981bb76528713
Priti <mail2pritipriya@gmail.com>
2025-09-11 11:31:50 +0530

    fix: user message

--- src/components/playground/ChatArea/Messages/MessageItem.tsx
+++ src/components/playground/ChatArea/Messages/MessageItem.tsx
@@ -80,14 +80,12 @@ const AgentMessage = ({ message }: MessageProps) => {
 
 const UserMessage = memo(({ message }: MessageProps) => {
   return (
-    <div className="flex items-start pt-4 text-start max-md:break-words">
-      <div className="flex flex-row gap-x-3">
-        <p className="flex items-center gap-x-2 text-sm font-medium text-muted">
-          <Icon type="user" size="sm" />
-        </p>
-        <div className="text-md rounded-lg py-1 font-geist text-secondary">
-          {message.content}
-        </div>
+    <div className="flex items-start gap-4 pt-4 text-start max-md:break-words">
+      <p className="flex-shrink-0">
+        <Icon type="user" size="sm" />
+      </p>
+      <div className="text-md rounded-lg font-geist text-secondary">
+        {message.content}
       </div>
     </div>
   )
